39fbdc45e50f153190fc4debce0bcbaf5c223ba5
Timothy Barao <tjb13b@my.fsu.edu>
2022-10-24 14:17:37 -0400

    Update DecompileAllFunctions.py
    Made it so certain functions are skipped/removed from decompilation

--- GhidraScripts/DecompileAllFunctions.py
+++ GhidraScripts/DecompileAllFunctions.py
@@ -13,7 +13,7 @@ ifc = DecompInterface()
 ifc.openProgram(program)
 
 # Get name of current Binary, set up Output Path
-name = currentProgram.getName()
+name = program.getName()
 out_dir = out_dir + name + '/'
 
 # Create Directory to store functions for current Binary if does not exist
@@ -21,16 +21,29 @@ if not os.path.exists(out_dir):
 	os.mkdir(out_dir)
 
 # Create Function Manager and get all functions loaded into Binary
-# True means forward direction of Binary
+# True means list functions in ascending address order
 fm = program.getFunctionManager()
-funcs = fm.getFunctions(True)
+funcs = fm.getFunctionsNoStubs(True)
+
+# List of functions that can be skipped/not decompiled
+skip_funcs = ["frame_dummy", 'tm_clones']
 
 # Iterate over Functions in Binary
-for func in funcs: 
+for func in funcs:
+    # Skip External Functions
+    if func.isExternal():
+        continue 
+
+    # Skip Function whose name begins with "_"
+    # Skip Functions in List of skippable functions
     FUNC_NAME = str(func.getName())
-    # Skip Function whose name begins with "_" 
-    if FUNC_NAME[0] != "_":
-        # Create file for decompiled function code to be saved to
-        with open(out_dir+func.getName()+".c", "w") as out_file:
-            results = ifc.decompileFunction(func, 0, ConsoleTaskMonitor())
-            out_file.write(results.getDecompiledFunction().getC())	
+    if FUNC_NAME[0] == '_' or any(f in FUNC_NAME for f in skip_funcs):
+        continue
+
+    # Decompile Function and get C Code as a string
+    results = ifc.decompileFunction(func, 0, ConsoleTaskMonitor())
+    d_code = results.getDecompiledFunction().getC()
+
+    # Write Decompiled code to file named the name of the function
+    with open(out_dir+func.getName()+".c", "w") as out_file:
+        out_file.write(d_code)	
