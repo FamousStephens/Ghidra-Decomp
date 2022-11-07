49578857ee7fd5e380878fa292058811283e172e
FamousStephens <ak17c@my.fsu.edu>
2022-11-07 14:59:46 -0500

    Class based structure

--- GhidraScripts/DecompileAllFunctions.py
+++ GhidraScripts/DecompileAllFunctions.py
@@ -1,7 +1,33 @@
+#Each function has a low level rep and a high 
+#
+
 from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
-
 import os
+
+class Function:
+    def __init__(self, lowFunc, decomp, tags, vars):
+        self.lowFunc = lowFunc 
+        self.name = self.lowFunc.getName()
+        self.decomp = decomp
+        self.symbolTable = decomp.getHighFunction().getLocalSymbolMap()
+        self.symbols = self.symbolTable.getSymbols()    #returns an iterator of symbols. La morte de Tim
+        self.tags = tags
+        self.vars = vars
+        self.hash = hash(self.name) #provide a hash for uniqueness
+        self.callConv = lowFunc.getCallingConventionName()
+
+
+    def returnCcode(self):
+        return self.decomp.getDecompiledFunction().getC()
+
+    def __str__(self):
+        vars_list = []
+        for s in self.symbols:
+            vars_list.append([s.getDataType().getName(),s.getName()])
+        funcStr = "{} {} ({})".format(self.callConv,self.name, vars_list)   
+        return funcStr 
+    
 
 # Assuming your current working directory is in GhidraBall
 cwd = os.getcwd()
@@ -26,11 +52,25 @@ fm = program.getFunctionManager()
 funcs = fm.getFunctionsNoStubs(True)
 
 # List of functions that can be skipped/not decompiled
-skip_funcs = ["frame_dummy", 'tm_clones']
+skip_funcs = ["frame_dummy", 'tm_clones'] 
+func_list = []
 
 # Iterate over Functions in Binary
 for func in funcs:
     # Skip External Functions
+    decomp_results = ifc.decompileFunction(func, 0, ConsoleTaskMonitor())
+    func_vars = func.getAllVariables()
+    func_tags = func.getTags() 
+    if func.isExternal():
+        msg = "{} is external, skipping".format(func.getName())
+        print(msg)
+    else:
+        f1 = Function(func, decomp_results, func_tags, func_vars)
+        func_list.append(f1)
+
+for i in func_list:
+    print(i)
+    """
     if func.isExternal():
         continue 
 
@@ -40,7 +80,7 @@ for func in funcs:
     if FUNC_NAME[0] == '_' or any(f in FUNC_NAME for f in skip_funcs):
         continue
 
-
+    
     func.setComment("This is a comment")
     func_c = func.getComment()
 
@@ -63,7 +103,6 @@ for func in funcs:
 
     #print(object_methods.toString)
     #print(symbols.toString)
-
     # Write Decompiled code to file named the name of the function
     with open(out_dir+func.getName()+".c", "w") as out_file:
         out_file.write(d_code)
@@ -80,3 +119,4 @@ for func in funcs:
         for s in symbols:
             out_file.write("DataType: " + s.getDataType().getName() + "\t Symbol: " + s.getName())
             out_file.write('\n')
+    """
