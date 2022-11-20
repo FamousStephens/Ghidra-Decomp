7b0bccd7487501c59055e3679a49752d14e2c7a6
Timothy Barao <tjb13b@my.fsu.edu>
2022-11-20 11:03:50 -0500

    Added JSON Export Functionality
    Added code to export Binary information to JSON File

--- GhidraScripts/DecompileAllFunctions.py
+++ GhidraScripts/DecompileAllFunctions.py
@@ -1,24 +1,130 @@
 from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
+from ghidra.program.model.pcode import HighFunctionDBUtil
+from ghidra.program.model.symbol import SourceType
+from ghidra.program.model.data import DataTypeManager
 
 import os
+import json
+
+class Function:
+    def __init__(self, lowFunc, decomp, tags, vars):
+
+        self.lowFunc = lowFunc 
+        self.decomp = decomp
+        self.highFunc = self.decomp.getHighFunction()
+
+        self.name = self.lowFunc.getName()
+        self.callConv = self.lowFunc.getCallingConventionName()
+        self.symbolTable = self.highFunc.getLocalSymbolMap()
+        self.symbols = self.symbolTable.getSymbols() 
+
+        #returns an iterator of symbols. La morte de Tim
+        self.tags = tags
+        self.vars = vars
+        self.hash = hash(self.name) #provide a hash for uniqueness
+        
+        self.st = SourceType.valueOf("USER_DEFINED")
+
+        self.hf_DB = HighFunctionDBUtil()
+        self.hf_DB.commitParamsToDatabase(self.highFunc, True, self.st)
+        self.hf_DB.commitLocalNamesToDatabase(self.highFunc, self.st)
+        self.hf_DB.commitReturnToDatabase(self.highFunc, self.st)
+
+        self.func_dict = {
+            "Function Name": self.name,
+            "Calling Convention": self.callConv,
+            "Tags": [],
+            "Symbols": []
+        }
+            #"Hash": self.hash,
+
+
+        for s in self.symbols:
+           self.func_dict["Symbols"].append({"Name" : s.getName(), "DataType" : s.getDataType().getName()})
+
+        for t in self.tags:
+           self.func_dict["Tags"].append([t.getName()])
+
+        self.symbols = self.symbolTable.getSymbols()
+
+
+    def returnCcode(self):
+        return self.decomp.getDecompiledFunction().getC()
+
+    def __str__(self):
+        vars_list = []
+        for s in self.symbols:
+            vars_list.append([s.getDataType().getName(),s.getName()])
+        self.symbols = self.symbolTable.getSymbols()
+        funcStr = "{} : {} {} ({})".format(hex(self.hash), self.callConv,self.name, vars_list)   
+        return funcStr 
+
+    def getSymbol(self, sym):
+        symTable = self.symbolTable.getSymbols() # Might still cause iterator issues
+        for s in symTable:
+            if s.getName() == sym:
+                self.symbols = self.symbolTable.getSymbols() 
+                return s
+
+    def getDB(self):
+        return self.hf_DB
+    
+    def getDict(self):
+        return self.func_dict
+
+    def update(self, newResults):
+        self.decomp = newResults
+        self.highFunc = newResults.getHighFunction()
+        self.lowFunc = self.highFunc.getFunction()
+        self.name = self.lowFunc.getName()
+        self.symbolTable = self.highFunc.getLocalSymbolMap()
+        self.symbols = self.symbolTable.getSymbols()
+
+        self.vars = self.lowFunc.getAllVariables()
+        self.func_tags = self.lowFunc.getTags() 
+
+        self.hf_DB = HighFunctionDBUtil()
+        self.hf_DB.commitParamsToDatabase(self.highFunc, True, self.st)
+        self.hf_DB.commitLocalNamesToDatabase(self.highFunc, self.st)
+        self.hf_DB.commitReturnToDatabase(self.highFunc, self.st) 
+
+def updateVar(decompInter, funcObj, var, newName=None, newDataType=None):
+    st = SourceType.valueOf("USER_DEFINED")
+    dtm = decompInter.getDataTypeManager()
+
+    varSymbol = funcObj.getSymbol(var)
+    
+    varSymbol.setNameLock(False) # Still does not allow name to be changed.
+
+    funcObj.getDB().updateDBVariable(varSymbol, newName, newDataType, st)
+
+    newResults = ifc.decompileFunction(funcObj.highFunc.getFunction(), 0, ConsoleTaskMonitor())
+    funcObj.update(newResults)
 
 # Assuming your current working directory is in GhidraBall
 cwd = os.getcwd()
 out_dir=cwd + "/DecompiledFuncs/"
+json_dir=cwd + "/JSONBinaries/"
 
 # Acquire current Binary loaded in Ghidra, and create Decompiler Interface
 program = getCurrentProgram()
 ifc = DecompInterface()
 ifc.openProgram(program)
 
+dtm = ifc.getDataTypeManager()
+
 # Get name of current Binary, set up Output Path
 name = program.getName()
 out_dir = out_dir + name + '/'
+#json_dir = json_dir + name + '/'
 
 # Create Directory to store functions for current Binary if does not exist
 if not os.path.exists(out_dir):
 	os.mkdir(out_dir)
+
+if not os.path.exists(json_dir):
+	os.mkdir(json_dir)
 
 # Create Function Manager and get all functions loaded into Binary
 # True means list functions in ascending address order
@@ -28,55 +134,45 @@ funcs = fm.getFunctionsNoStubs(True)
 # List of functions that can be skipped/not decompiled
 skip_funcs = ["frame_dummy", 'tm_clones']
 
+bin_dict = {
+    "Binary Name" : name,
+    "Functions" : {}}
+
 # Iterate over Functions in Binary
 for func in funcs:
-    # Skip External Functions
-    if func.isExternal():
-        continue 
+    if func.getName() in skip_funcs:
+        msg = "{} is external, skipping".format(func.getName())
+        print(msg)
+    else:
+        decomp_results = ifc.decompileFunction(func, 0, ConsoleTaskMonitor())
+        func_vars = func.getAllVariables()
+        func_tags = func.getTags() 
+        f1 = Function(func, decomp_results, func_tags, func_vars)
+        func_list.append(f1)
 
-    # Skip Function whose name begins with "_"
-    # Skip Functions in List of skippable functions
-    FUNC_NAME = str(func.getName())
-    if FUNC_NAME[0] == '_' or any(f in FUNC_NAME for f in skip_funcs):
-        continue
+        bin_dict["Functions"][f1.hash] = f1.getDict()
+
+        with open(out_dir+func.getName()+".c", "w") as out_file:
+            out_file.write(f1.decomp.getDecompiledFunction().getC())
+
+with open(json_dir + name[:-4] +".json", "w") as out_file:
+    json.dump(bin_dict, out_file, indent=3, sort_keys=True)
 
 
-    func.setComment("This is a comment")
-    func_c = func.getComment()
 
-    func_tags = func.getTags()
-    func.addTag("I am a Tag")
+'''
+Old Code to update Varaible in Function
 
-    func_vars = func.getAllVariables()
-    for v in func_vars:
-        v.setComment("Var Name: " + v.getName())
+for i in func_list:
+    print(i)
+    if i.name == "main" and program.getName() == "sample1.exe":
 
-    # Decompile Function and get C Code as a string
-    results = ifc.decompileFunction(func, 0, ConsoleTaskMonitor())
-    d_code = results.getDecompiledFunction().getC()
+        dataTypeObj = dtm.findBaseType("uint", None)
 
-    sm = results.getHighFunction().getLocalSymbolMap()
-    symbols = sm.getSymbols()
+        with open(out_dir+func.getName()+"_UpdateTest.c", "w") as out_file:
+            out_file.write(i.decomp.getDecompiledFunction().getC())
+            updateVar(ifc, i, "uVar1", "uVar7", dataTypeObj)
+            out_file.write(i.decomp.getDecompiledFunction().getC())
 
-    #object_methods = [method_name for method_name in dir(symbols)
-    #              if callable(getattr(symbols, method_name))]
-
-    #print(object_methods.toString)
-    #print(symbols.toString)
-
-    # Write Decompiled code to file named the name of the function
-    with open(out_dir+func.getName()+".c", "w") as out_file:
-        out_file.write(d_code)
-        out_file.write("=-=-=-= TAGS =-=-=-=\n")
-        for t in func_tags:
-            out_file.write(t.getName())
-            out_file.write('\n')
-        out_file.write("\n=-=-=-= func_VARS =-=-=-=\n")
-        for v in func_vars:
-            out_file.write("DataType: " + v.getDataType().getName() + "\t Symbol: " + v.getSymbol().getName())
-            out_file.write('\n')
-
-        out_file.write("\n=-=-=-= SM =-=-=-=\n")
-        for s in symbols:
-            out_file.write("DataType: " + s.getDataType().getName() + "\t Symbol: " + s.getName())
-            out_file.write('\n')
+        print(i)
+'''
