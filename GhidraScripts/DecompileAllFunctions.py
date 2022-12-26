fcc59da5a3a33dc7de1c3335fdaacb9a4543af15
FamousStephens <65720791+FamousStephens@users.noreply.github.com>
2022-12-25 19:21:28 -0500

    Needed to fix some issues left over from merge

--- GhidraScripts/DecompileAllFunctions.py
+++ GhidraScripts/DecompileAllFunctions.py
@@ -104,81 +104,6 @@ def updateVar(decompInter, funcObj, var, newName=None, newDataType=None):
     newResults = ifc.decompileFunction(funcObj.highFunc.getFunction(), 0, ConsoleTaskMonitor())
     funcObj.update(newResults)
 
-class Function:
-    def __init__(self, lowFunc, decomp, tags, vars):
-        self.lowFunc = lowFunc 
-        self.highFunc = decomp.getHighFunction()
-        self.name = self.lowFunc.getName()
-        self.decomp = decomp
-        self.symbolTable = self.highFunc.getLocalSymbolMap()
-        self.symbols = self.symbolTable.getSymbols()    #returns an iterator of symbols. La morte de Tim
-        self.tags = tags
-        self.vars = vars
-        self.hash = hash(self.name) #provide a hash for uniqueness
-        self.callConv = lowFunc.getCallingConventionName()
-
-        self.st = SourceType.valueOf("USER_DEFINED")
-
-        self.hf_DB = HighFunctionDBUtil()
-        self.hf_DB.commitParamsToDatabase(self.highFunc, True, self.st)
-        self.hf_DB.commitLocalNamesToDatabase(self.highFunc, self.st)
-        self.hf_DB.commitReturnToDatabase(self.highFunc, self.st)
-
-
-    def returnCcode(self):
-        return self.decomp.getDecompiledFunction().getC()
-
-    def __str__(self):
-        vars_list = []
-        for s in self.symbols:
-            vars_list.append([s.getDataType().getName(),s.getName()])
-        self.symbols = self.symbolTable.getSymbols()
-        funcStr = "{} : {} {} ({})".format(hex(self.hash), self.callConv,self.name, vars_list)   
-        return funcStr 
-
-    def getSymbol(self, sym):
-        symTable = self.symbolTable.getSymbols() # Might still cause iterator issues
-        for s in symTable:
-            print("-=--=-==-=-=-=--=", s.getName(), sym)
-            if s.getName() == sym:
-                self.symbols = self.symbolTable.getSymbols() 
-                print(s.getName())
-                return s
-
-    def getDB(self):
-        return self.hf_DB
-
-    def update(self, newResults):
-        self.decomp = newResults
-        self.highFunc = newResults.getHighFunction()
-        self.lowFunc = self.highFunc.getFunction()
-        self.name = self.lowFunc.getName()
-        self.symbolTable = self.highFunc.getLocalSymbolMap()
-        self.symbols = self.symbolTable.getSymbols()
-
-        self.vars = self.lowFunc.getAllVariables()
-        self.func_tags = self.lowFunc.getTags() 
-
-        self.hf_DB = HighFunctionDBUtil()
-        self.hf_DB.commitParamsToDatabase(self.highFunc, True, self.st)
-        self.hf_DB.commitLocalNamesToDatabase(self.highFunc, self.st)
-        self.hf_DB.commitReturnToDatabase(self.highFunc, self.st) 
-
-
-def updateVar(decompInter, funcObj, var, newName=None, newDataType=None):
-    st = SourceType.valueOf("USER_DEFINED")
-    dtm = decompInter.getDataTypeManager()
-
-    varSymbol = funcObj.getSymbol(var)
-    
-    varSymbol.setNameLock(False) # Still does not allow name to be changed.
-
-    funcObj.getDB().updateDBVariable(varSymbol, newName, newDataType, st)
-
-    newResults = ifc.decompileFunction(funcObj.highFunc.getFunction(), 0, ConsoleTaskMonitor())
-    funcObj.update(newResults)
-    
-
 # Assuming your current working directory is in GhidraBall
 cwd = os.getcwd()
 out_dir=cwd + "/DecompiledFuncs/"
@@ -198,11 +123,10 @@ out_dir = out_dir + name + '/'
 
 # Create Directory to store functions for current Binary if does not exist
 if not os.path.exists(out_dir):
-    print("Why are we not making a directory?")
     os.mkdir(out_dir)
 
 if not os.path.exists(json_dir):
-	os.mkdir(json_dir)
+    os.mkdir(json_dir)
 
 # Create Function Manager and get all functions loaded into Binary
 # True means list functions in ascending address order
@@ -223,6 +147,7 @@ bin_dict = {
     "Functions" : {}}
 #loop through functions, if external, skip 
 # Iterate over Functions in Binary
+func_list = []
 for func in funcs:
     if func.isThunk() and func.getThunkedFunction(True).isExternal():
         print("Skipping External Function: {}".format(func.getName()))
@@ -234,9 +159,13 @@ for func in funcs:
         func_vars = func.getAllVariables()
         func_tags = func.getTags() 
         f1 = Function(func, decomp_results, func_tags, func_vars)
+
         bin_dict["Functions"][f1.hash] = f1.getDict()
+        
         with open(out_dir+func.getName()+".c", "w") as out_file:
             out_file.write(f1.decomp.getDecompiledFunction().getC())
+
+        func_list.append(f1)
 
 with open(json_dir + name[:-4] +".json", "w") as out_file:
     json.dump(bin_dict, out_file, indent=3, sort_keys=True)
@@ -259,7 +188,7 @@ for i in func_list:
 
         print(i)
 '''
-        func_list.append(f1)
+    
 
 for i in func_list:
     print(i)
