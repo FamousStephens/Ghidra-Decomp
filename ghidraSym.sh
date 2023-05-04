ca6bc824d75a0efb2d619ff56b2e5b557fc8b754
FamousStephens <ak17c@my.fsu.edu>
2023-05-04 11:08:16 -0400

    Added conditional statements to creating symlinks

--- ghidraSym.sh
+++ ghidraSym.sh
@@ -1,3 +1,21 @@
-#Substitute your path to Ghidra 
-ln -s /home/alexandros/Documents/ghidra_10.1.5_PUBLIC/ ghidra_10.1.5_PUBLIC
-mkdir DecompiledFuncs
+folder1="DecompiledFuncs"
+folder2="GhidraProjects"
+folder3="ghidra_10.1.5_PUBLIC"
+
+if [ -d "$folder3" ]; then
+    echo "$folder3 already exists"
+else
+    #Substitute with your own path to Ghidra 
+    ln -s /home/alexandros/Documents/ghidra_10.1.5_PUBLIC/ ghidra_10.1.5_PUBLIC
+fi
+if [ -d "$folder1" ]; then
+    echo "$folder1 already exists"
+else
+    mkdir DecompiledFuncs
+fi
+
+if [ -d "$folder2" ]; then
+    echo "$folder2 already exists"
+else
+    mkdir $folder2
+fi
