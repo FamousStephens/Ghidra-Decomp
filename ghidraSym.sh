66ce55aca42cbf98a2b3ffaf18a109b0a1627056
FamousStephens <ak17c@my.fsu.edu>
2023-05-04 12:26:33 -0400

    More changes to ghidraSym.sh

--- ghidraSym.sh
+++ ghidraSym.sh
@@ -6,14 +6,13 @@ if [ -d "$folder3" ]; then
     echo "$folder3 already exists"
 else
     #Substitute with your own path to Ghidra 
-    ln -s /home/alexandros/Documents/ghidra_10.1.5_PUBLIC/ ghidra_10.1.5_PUBLIC
+    ln -s /home/alexandros/Documents/ghidra_10.1.5_PUBLIC/ $folder3
 fi
 if [ -d "$folder1" ]; then
     echo "$folder1 already exists"
 else
     mkdir DecompiledFuncs
 fi
-
 if [ -d "$folder2" ]; then
     echo "$folder2 already exists"
 else
